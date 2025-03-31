b498611f672548a0385ae040091be67cf4cd2f46
Ralph Uy <areaoear@gmail.com>
2025-03-30 16:05:31 -1000

    list contacts page mockup finished in 24 minutes

---
+++ src/components/ContactCard.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import Card from 'react-bootstrap/Card';
+// eslint-disable-next-line import/extensions
+import { Contact } from '@/lib/validationSchemas';
+
+/* Renders a single row in the List Stuff table. See list/page.tsx. */
+const ContactCard = ({ contact }: { contact: Contact }) => (
+  <Card className="h-100">
+    <Card.Header>
+      <Card.Img variant="top" src={contact.image} style={{ width: '75px' }} />
+      <Card.Title>{`${contact.firstName} ${contact.lastName}`}</Card.Title>
+      <Card.Subtitle className="mb-2 text-muted">{contact.address}</Card.Subtitle>
+    </Card.Header>
+    <Card.Body>
+      <Card.Text>{contact.description}</Card.Text>
+    </Card.Body>
+  </Card>
+
+);
+
+export default ContactCard;
